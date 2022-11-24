5430c271c6b5c68a96c4e742c86a3f64ecb2568d
Artur Passos Moreira <103152018+ArturPassosMoreira@users.noreply.github.com>
2022-11-24 01:07:49 +0000

    Acessando banco de dados

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 #importa 
 from model.Pessoa import Pessoa
+from database.Database import Database
+from dao.PessoaDAO import PessoaDAO
+
 #Exemplo de uso
 artur = Pessoa(1, "Artur Moreira")
 print (artur)
 #Quero mostrar so o nome 
 print(artur.nome)
+#Chama o objeto no banco de dados
+db = Database()
+#Instancia o objeto
+pessoaDAO = PessoaDAO(db.conexao, db.cursor)
+
+#Quero carregar uma lista com o que veio do banco de dados
+pessoas = pessoaDAO.getAll(orderBy=True)
+for pessoa in pessoas:
+  print(pessoa)
